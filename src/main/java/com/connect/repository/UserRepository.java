3cf1830696293bffe8bdde4b71b6bc105007166f
Joshua The Joh <43902554+joshua134@users.noreply.github.com>
2024-07-23 11:55:16 +0300

    Update UserRepository.java

--- src/main/java/com/connect/repository/UserRepository.java
+++ src/main/java/com/connect/repository/UserRepository.java
@@ -11,14 +11,32 @@ import com.connect.enums.Gender;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	User findByEmail(String email);
+	/** 
+ 	* @Query("SELECT u FROM User u WHERE u.email = :mail")
+  	* User findByEmail(@Param("mail")String mail);
+   	**/
+	
 	User findByUsername(String uname);
+	/** @Query("SELECT u FROM User u WHERE u.username = :username"); 
+ 	* User findByUsername(@Param("username") String username); **/
 	
 	Page<User> findAll(Pageable p);
-	Page<User> findByGender(Gender gender, Pageable p);
+	/** @Query("SELECT u FROM User u") Page<User> findAll(Pageable p); **/
 
 	Page<User> findAllByGender(Gender g, Pageable p);
+	// or
+	/**
+ 	*	@Query("SELECT u FROM User u WHERE u.gender  = :gender")
+  	*	Page<User> findAllByGender(Gender g, Pageable p);
+  	*/
+	
 	Page<User> findAllByRole(Role r, Pageable p);
-
+	// or
+	/**
+ 	*	Query("SELECT u FROM User u WHERE u.role = :role")
+  	*	Page<User> findAllByRole(Role r, Pageable p);
+   	*/
+	
 	Page<Post> findAllPostsByUser(User u, Pageable p);
 	// or
 	/**
